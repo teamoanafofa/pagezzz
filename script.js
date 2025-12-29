c19300c0ee7093ee62c5133191cd626f1a153057
teamoana <mtxexaurir@gmail.com>
2025-12-29 20:07:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,32 +7,30 @@ const popup = document.getElementById("popup");
 const fechar = document.getElementById("fechar");
 
 playBtn.onclick = () => {
-    musica.play();
+  musica.play();
 };
 
 nao.onclick = () => {
-    nao.style.position = "absolute";
-    nao.style.left = Math.random() * 80 + "%";
-    nao.style.top = Math.random() * 80 + "%";
+  nao.style.position = "absolute";
+  nao.style.left = Math.random()*80 + "%";
+  nao.style.top = Math.random()*80 + "%";
 };
 
 sim.onclick = () => {
-    popup.style.display = "flex";
+  popup.style.display = "flex";
 };
 
 fechar.onclick = () => {
-    popup.style.display = "none";
+  popup.style.display = "none";
 };
 
 const inicio = new Date("2025-11-19T00:00:00");
 
-setInterval(() => {
-    const agora = new Date();
-    const diff = agora - inicio;
-
-    const dias = Math.floor(diff / (1000*60*60*24));
-    const horas = Math.floor((diff / (1000*60*60)) % 24);
-    const minutos = Math.floor((diff / (1000*60)) % 60);
-
-    cronometro.innerText = `${dias} dias ${horas}h ${minutos}min juntos 💕`;
-}, 1000);
+setInterval(()=>{
+  const agora = new Date();
+  const diff = agora - inicio;
+  const dias = Math.floor(diff/(1000*60*60*24));
+  const horas = Math.floor(diff/(1000*60*60)%24);
+  const min = Math.floor(diff/(1000*60)%60);
+  cronometro.innerText = `${dias} dias ${horas}h ${min}min juntos 💕`;
+},1000);
